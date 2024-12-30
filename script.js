8533628e5b911050d1e9a2b2e29ce22f8df75b2d
Muhammad Hasnain <h.kamran13811@gmail.com>
2024-12-30 10:47:46 +0500

    Ver 3

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+function filterVideos(category) {
+    const allButtons = document.querySelectorAll('.category-btn');
+    allButtons.forEach(btn => btn.classList.remove('active'));
+  
+    const selectedButton = Array.from(allButtons).find(btn => 
+      btn.textContent.toLowerCase() === category
+    );
+    selectedButton.classList.add('active');
+  
+    const allCards = document.querySelectorAll('.project-card');
+    allCards.forEach(card => {
+      if (card.dataset.category === category || category === 'all') {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  }
